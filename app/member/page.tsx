62b913f1fb6d061daedae52f575c582fe760fdb9
orogerli <157654201+orogerli@users.noreply.github.com>
2024-01-31 18:36:17 -0500

    Update page.tsx

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -39,7 +39,7 @@ const Blog = () => {
   return (
     <>
 
-      <section className="pt-[120px] pb-[25px]">
+      <section className="pt-[120px] pb-[0px]">
         <div className="container">
 
         <h5 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
