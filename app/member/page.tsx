ab51b5adc54697b0430e23f15f5ec5fa652af8eb
orogerli <157654201+orogerli@users.noreply.github.com>
2024-02-05 17:51:16 -0500

    Update page.tsx

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -17,6 +17,7 @@ const Blog = () => {
 
   const projectmanager=[
     'Jacob Ruby (NSU, Pre-Opt)',
+'Nada Bleibel (NSU, pre-Opt, Independent Study)',
 'Srujan Miryala (NSU, Pre-Med, Independent Study)',
 
   ]
@@ -24,7 +25,6 @@ const Blog = () => {
   const researchassit=[
     'Aliya DeJesus (NSU, pre-Opt)',
 'Murielle Cayemitte (NSUCO)',
-'Nada Bleibel (NSU, pre-Opt, Independent Study)',
 'Nimisha Kumar (NSU, pre-Med)',
 'Riya Vadalia (NSU, Pre-Med)',
 'Taylor Zinkler (UF, Pre-DPsy)',
