876d12eaafd52116a4ee21f156c5651c1d9e492d
TrmiDo <111090515+TrmiDo@users.noreply.github.com>
2024-02-07 15:55:35 -0500

    Create page2.tsx

---
+++ app/signup/page2.tsx
@@ -0,0 +1,188 @@
+import SharePost from "@/components/Blog/SharePost";
+import TagButton from "@/components/Blog/TagButton";
+import Image from "next/image";
+
+const SignupPage = () => {
+  
+  return (
+    <>
+      <section className="pt-[150px] pb-[120px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            <div className="w-full px-4 lg:w-8/12">
+              <div>
+                
+                <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+                
+                
+                <h3 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                Amblyopia (“Lazy Eye”) Treatment Study
+                  </h3>
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  Children and adults with amblyopia are invited to participate in a clinical research trial conducted at the Nova Southeastern University College of Optometry. This research is set out to develop and evaluate a new vision training approach for improving lazy eye
+                  </p>
+                  
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  In the study, participants will be asked to look at a series of patterns on a monitor screen and make simple perceptual judgments. If you or your child has amblyopia and would like to participate, please contact the Principal Investigator</p>
+                </div>
+
+
+
+
+                <h3 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                3D Vision Study
+                  </h3>
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  Adults with normal vision are invited to participate in a 3D vision study conducted at the Nova Southeastern University College of Optometry.
+                  </p>
+                  
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  In the study, participants will be asked to look at a series of patterns on a monitor screen and make simple perceptual judgments. If you have 20/20 vision and would like to participate, please contact the Principal Investigator.</p>
+                  </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-0 right-0 z-[-1] opacity-30 lg:opacity-100">
+          <svg
+            width="450"
+            height="556"
+            viewBox="0 0 450 556"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="277"
+              cy="63"
+              r="225"
+              fill="url(#paint0_linear_25:217)"
+            />
+            <circle
+              cx="17.9997"
+              cy="182"
+              r="18"
+              fill="url(#paint1_radial_25:217)"
+            />
+            <circle
+              cx="76.9997"
+              cy="288"
+              r="34"
+              fill="url(#paint2_radial_25:217)"
+            />
+            <circle
+              cx="325.486"
+              cy="302.87"
+              r="180"
+              transform="rotate(-37.6852 325.486 302.87)"
+              fill="url(#paint3_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="184.521"
+              cy="315.521"
+              r="132.862"
+              transform="rotate(114.874 184.521 315.521)"
+              stroke="url(#paint4_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="356"
+              cy="290"
+              r="179.5"
+              transform="rotate(-30 356 290)"
+              stroke="url(#paint5_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="191.659"
+              cy="302.659"
+              r="133.362"
+              transform="rotate(133.319 191.659 302.659)"
+              fill="url(#paint6_linear_25:217)"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_25:217"
+                x1="-54.5003"
+                y1="-178"
+                x2="222"
+                y2="288"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <radialGradient
+                id="paint1_radial_25:217"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(17.9997 182) rotate(90) scale(18)"
+              >
+                <stop offset="0.145833" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0.08" />
+              </radialGradient>
+              <radialGradient
+                id="paint2_radial_25:217"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(76.9997 288) rotate(90) scale(34)"
+              >
+                <stop offset="0.145833" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0.08" />
+              </radialGradient>
+              <linearGradient
+                id="paint3_linear_25:217"
+                x1="226.775"
+                y1="-66.1548"
+                x2="292.157"
+                y2="351.421"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint4_linear_25:217"
+                x1="184.521"
+                y1="182.159"
+                x2="184.521"
+                y2="448.882"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="white" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint5_linear_25:217"
+                x1="356"
+                y1="110"
+                x2="356"
+                y2="470"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="white" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint6_linear_25:217"
+                x1="118.524"
+                y1="29.2497"
+                x2="166.965"
+                y2="338.63"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SignupPage;
