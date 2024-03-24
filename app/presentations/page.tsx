8fc992b04caf0ddde9ac0804fb0302f2afd5bd12
orogerli <157654201+orogerli@users.noreply.github.com>
2024-03-24 02:04:58 -0400

    Update page.tsx

--- app/presentations/page.tsx
+++ app/presentations/page.tsx
@@ -137,7 +137,7 @@ const  BlogDetailsPage = () => {
                   </div>
 
                   <h3 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
-                    2020-Current
+                    2021-Current
                   </h3>
                   <ul className="mb-10 list-inside list-disc text-body-color">
                   {item2029.map((item, index) => (
