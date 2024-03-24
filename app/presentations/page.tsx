536b0a2417133c2d463392acd64d11a5470d6cb0
orogerli <157654201+orogerli@users.noreply.github.com>
2024-03-24 02:02:58 -0400

    Update page.tsx

--- app/presentations/page.tsx
+++ app/presentations/page.tsx
@@ -10,10 +10,12 @@ const  BlogDetailsPage = () => {
   'Chung STL, Li RW, Li BZ, Lei Q, Levi DM. (2022) Local analysis reveals lower light sensitivity and thicker retinal layers of the amblyopic eyes near the fovea. ARVO, Denver, CO',
   'Cheong AMY, Lam HY, Lyu AQ, Abel LA, Chan AY, Cheung YF, Lee PH, Li LSW, Li R, Li RW. (2021) Pursuit eye movement of Parkinson’s disease – pilot study. American Academy of Optometry, Boston, MA',
   'Chung STL, Li RW, Agaoglu MN, Tiruveedhula P, Roorda A. (2021) Binocular properties of fixational eye movements as assessed using a high-resolution binocular scanning laser ophthalmoscope. ARVO, Baltimore, MD',
-  'Chung STL, Li RW, Tiruveedhula P, Agaoglu MN, Levi DM, Roorda A. (2020) Two eyes are better than one: the importance of binocular visual input in eye fixation in amblyopia. Vision Sciences Society, St. Pete Beach, Florida.']
+
+  ]
   
   const items2019 =[
-    'Antonucci MA, Leung JT, Li BZ, Kee CS, Bui JK, Feldmar B, Levi DM, Li RW. (2019) Improving meridional acuity deficits in astigmatism-related amblyopia by grating acuity training. American Academy of Optometry, Orlando, FL',
+'Chung STL, Li RW, Tiruveedhula P, Agaoglu MN, Levi DM, Roorda A. (2020) Two eyes are better than one: the importance of binocular visual input in eye fixation in amblyopia. Vision Sciences Society, St. Pete Beach, Florida.'   
+'Antonucci MA, Leung JT, Li BZ, Kee CS, Bui JK, Feldmar B, Levi DM, Li RW. (2019) Improving meridional acuity deficits in astigmatism-related amblyopia by grating acuity training. American Academy of Optometry, Orlando, FL',
 'Li BZ, Tran KD, Ngo CV, Antonucci MA, Bui JK, Chau DD, Levi DM, Li RW. (2019) Stereoscopic 3D videogame play boosts stereoacuity, but not contrast sensitivity. American Academy of Optometry, Orlando, FL',
 'Levi DM, Li RW. (2019) Playing 3-dimensional (3D), but not 2D video games can improve stereoacuity in neurotypical observers. Vision Sciences Society, St. Pete Beach, Florida.',
 'Leung TW, Antonucci MA, Li BZ, Wong C, Chan KH, Feldmar B, Tran KD, Kee CS, Li RW. (2019) Improving Grating Acuity in Adult Amblyopia by Perceptual Learning. ARVO, Vancouver, Canada.',
@@ -29,29 +31,6 @@ const  BlogDetailsPage = () => {
 'Tran KD, Antonucci MA, Bui JK, Levi DM, Li RW. (2016) Towards developing a new binocular technique for amblyopia treatment using stereoscopic “3D” video games. American Academy of Optometry, Anaheim, CA. [Talk, Hot topics: Binocular Vision]',
 'Tran KD, Li BZ, Antonucci MA, Ngo CV, Chat S, Kee, C, Levi DM, Li RW. (May 2016) Astigmatic blur alters the spatial frequency tuning characteristics of stereopsis. ARVO, Seattle, WA, USA.',
 'Cheong AMY, Lam HY, Tsang WWN, Leat SJ, Li RW. (May 2016) Slow-pace videogames are more effective than fast-pace videogames in enhancing visual attention in older adults. ARVO, Seattle, WA, USA.',
-'Tran KD, Bui, J, Yang E, Moghtaderi A, Ngo CV, Chat S, Kee, C, Levi DM, Li RW. (Oct 2015) Effect of induced astigmatic blur on the spatial frequency tuning of stereopsis. American Academy of Optometry, New Orleans, Louisiana ,USA',
-'Cheong AMY, Lam HY, Siong KH, Ting PWK, Tsang WWN, Leat SJ, Li RW. (Oct 2015) Dynamic visual acuity in community dwelling older adults. American Academy of Optometry, New Orleans, Louisiana ,USA',
-'Cheong AMY, Lam HY, Siong KH, Chan HHL, Tsang WWN, Li RW, Leat SJ. (Oct 2015) Effects of eye movements on postural sway in community dwelling older adults. American Academy of Optometry, New Orleans, Louisiana ,USA',
-'Webb BS, McGraw, PV, Levi DM, Li RW. (Aug, 2015) The perceptual costs and benefits of learning to multitask. European Conference on Visual Perception, Liverpool, UK',
-'Leung TW, Li RW & Kee CS. (July 2015) The effects of anti-blue light lenses on visual and biological function. Macular Carotenoids Conference. Cambridge, UK.',
-'Tran, KD, Ngo C., Bui, J, Yang, E, Levi DM & Li RW. (May 2015) Enhancing tolerance of image motion through perceptual learning: Static and dynamic bisection acuity. ARVO, Denver, CO, USA.',
-'Leung TW, Li RW, & Kee CS (May 2015) The retinal thickness and anterior/posterior ocular biometry in Chinese myopic eyes with and without high astigmatism. ARVO, Denver, CO, USA.',
-'Tran K, Ngo C, Yang E, Bui J, Chat S, Dion Z, Levi DM, Li RW. (Nov 2014) Enhancing tolerance of image motion through perceptual learning: Static and dynamic bisection acuity. American Academy of Optometry, Denver, CO, USA.',
-'Chung STL, Kumar G, Li RW & Levi DM (May 2014) Fixation Stability and fixational eye movements in amblyopia. ARVO, Orlando, FL, USA.',
-'* Wu T, Craven A, Tran T, Tran K, Levi DM & Li RW (May 2014) Enhancing coarse-to-fine stereo vision by perceptual learning: An asymmetric transfer across spatial frequency spectrum. ARVO, Orlando, FL, USA. ',
-'* So, K., Wu, T., Tran, T., Craven, A., Tran, K., Levi DM & Li RW (May 2014) Interocular acuity differences alter the size tuning function of stereopsis. ARVO, Orlando, FL, USA.',
-'*Craven, A., Tran, K., Wu, T., Tran, T., So, K., Levi DM & Li RW (Oct 2013) Enhancing coarse-to-fine stereo vision by perceptual learning: An asymmetric transfer across spatial frequency spectrum. American Academy of Optometry, Seattle, WA, USA. ',
-'Wu, T., Tran, T., Tran, K., So, K., Craven, A., Levi DM & Li RW (Oct 2013) Interocular acuity differences alter the size tuning function of stereopsis. American Academy of Optometry, Seattle, WA, USA.',
-'Ngo C, Levi DM & Li RW (Oct 2013) Video game experience improves temporal processing in adult amblyopia. American Academy of Optometry, Seattle, WA, USA. [Paper]',
-'Tran, T., So, K., Nguyen C., Leung TW, Chat, S. W., Levi DM, & Li RW (May 2013) Enhancing stereoacuity through perceptual learning in normal vision: Specificity for spatial frequency and orientation. ARVO, Seattle, WA, USA.',
-'Craven, A., Truyet, T., Gustafson, K., Chat, S. W., Levi DM & Li RW (May 2013) Interocular acuity differences alter the spatial frequency tuning of stereopsis. ARVO, Seattle, WA, USA.',
-'Ngo C, Truyet, T., Nguyen C., Levi DM, & Li RW (May 2013) Video game experience improves temporal processing in adult amblyopia. ARVO, Seattle, WA, USA.',
-'* Craven, A., Gustafson, K., Truyet, T., Chat, S. W. Levi DM, & Li RW (Oct 2012) Interocular acuity differences alter the spatial frequency tuning of stereopsis. American Academy of Optometry. Phoenix, AZ, USA',
-'* Truyet, T., Nguyen C., Leung, T.W., Chat, S. W., Levi DM, & Li RW (Oct 2012) Enhancing stereoacuity through perceptual learning in normal vision: Specificity for spatial frequency and orientation. American Academy of Optometry. Phoenix, AZ, USA  [Paper]',
-'Li RW, Gustafson, K., Chat, S., & Levi DM (May 2012) Towards establishing a new treatment protocol for improving stereovision in patients with amblyopia. ARVO, Fort Lauderdale, Florida, USA.',
-'Chung, S. T. L., Li RW, Levi, D. M (May 2012) A "fuller" report on mislocation errors in visual crowding. Vision Sciences Society. Naple, Florida, USA',
-'Astle, A. T., Li RW, McGraw, P. V., Webb, B. S. & Levi,  D. M. (Dec 2011) The magnitude of perceptual learning is equated when stimuli are scaled according to cortical magnification factor. Applied Vision Association meeting AVA. York, UK',
-
   ]
 
 
