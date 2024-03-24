49d15a30cacfb76fd9b8428bb9224dc18f1f1eec
orogerli <157654201+orogerli@users.noreply.github.com>
2024-03-23 20:12:57 -0400

    Update page.tsx

--- app/award/page.tsx
+++ app/award/page.tsx
@@ -59,7 +59,7 @@ const BlogDetailsPage = () => {
               <div>
                 
                 <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
-                <h3 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-lg sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                <h3 className="font-lg mb-10 font-bold leading-tight text-black dark:text-white sm:text-lg sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
                   Awards and Honors
                   </h3>
                   
