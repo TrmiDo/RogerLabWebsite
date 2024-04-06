46a2260bbf041ad137e306e0740db4d773c61122
orogerli <157654201+orogerli@users.noreply.github.com>
2024-04-05 23:23:38 -0400

    Update page.tsx

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const BlogDetailsPage = () => {
   const item2029=[
-    'Li RW, Li BZ, Chat SW, Patel SS, Chung STL & Levi DM (2024, in revision) ',
+    'Li RW, Li BZ, Chat SW, Patel SS, Chung STL & Levi DM (2024, in press) ',
 'Leung TW, Li RW, Kee CS (2023) Brief Adaptation to Astigmatic Blur Reduces Meridional Anisotropy in Contrast Sensitivity. Investigative Ophthalmology and Visual Science, 64, e4',
 'Leung TW, Li RW, Kee CS (2021) Meridional Anisotropy of Foveal and Peripheral Resolution Acuity in Adults with Emmetropia, Myopia and Astigmatism. Investigative Ophthalmology and Visual Science, 62, e11',
 'Levi DM, Li RW, Silver ME, Chung ST (2020). Sequential perceptual learning of letter identification and uncrowding in normal peripheral vision: Effects of task, training order, and cholinergic enhancement. Journal of Vision',
