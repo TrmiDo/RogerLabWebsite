8f816055395c5608b664a816510d87f0ee5d3d0a
orogerli <157654201+orogerli@users.noreply.github.com>
2025-03-31 19:37:05 -0400

    Update page.tsx

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -92,7 +92,7 @@ const Blog = () => {
           </ul>
 
           <h3 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[20px]">
-           Research Assistants
+           Research & Teaching Assistants
         </h3>
           <ul className="mb-10 list-inside list-disc text-body-color">
           {researchassit.map((item, index) => (
